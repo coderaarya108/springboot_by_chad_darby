954a41deb0b6f71aa4597a0ef04677145e32facc
premmyadav@gmail.com <premmyadav@gmail.com>
2026-04-30 15:24:04 +0530

    Change to use encrypted password for users . We just need to change the schema and append {bcrypt} in the password field followed by encryped password . No other java code change is needed .

--- 06-spring-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 06-spring-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -5,14 +5,64 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig  {
 
+    private static final String ROLE_EMPLOYEE = "EMPLOYEE";
+    private static final String ROLE_MANAGER = "MANAGER";
+
+    // add support for jdbc .  no more hard coded users
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource datasource) {
+
+        return new JdbcUserDetailsManager(datasource);
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) {
+
+
+        try {
+            String employeeIdEndpoint = "/api/employees/**";
+
+            http.authorizeHttpRequests(configurer -> configurer
+                    .requestMatchers(HttpMethod.GET, "/api/employees").hasRole(ROLE_EMPLOYEE)
+                    .requestMatchers(HttpMethod.GET, employeeIdEndpoint).hasRole(ROLE_EMPLOYEE)
+                    .requestMatchers(HttpMethod.POST, "/api/employees").hasRole(ROLE_MANAGER)
+                    .requestMatchers(HttpMethod.PUT, employeeIdEndpoint).hasRole(ROLE_MANAGER)
+                    .requestMatchers(HttpMethod.PATCH, employeeIdEndpoint).hasRole(ROLE_MANAGER)
+                    .requestMatchers(HttpMethod.DELETE, employeeIdEndpoint).hasRole("ADMIN")
+            );
+
+            // use HTTP Basic authentication
+            http.httpBasic(Customizer.withDefaults());
+
+            // disable CSRF (cross site request forgery)
+            // in general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+            http.csrf(csrf -> csrf.disable());
+
+            return http.build();
+        } catch (Exception e) {
+            throw new IllegalStateException("Failed to build Spring Security Filter chain", e);
+        }
+    }
+
+
+
+
+
+
+
+
+
+    /*
     private static final String ROLE_EMPLOYEE = "EMPLOYEE";
     private static final String ROLE_MANAGER = "MANAGER";
 
@@ -69,4 +119,6 @@ public class DemoSecurityConfig  {
         }
 
     }
+    */
+
 }
