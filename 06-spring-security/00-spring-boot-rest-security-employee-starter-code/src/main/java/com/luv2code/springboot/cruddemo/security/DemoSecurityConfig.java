4ab42b8a82ea90dce616de04be650f4ed59bd86e
premmyadav@gmail.com <premmyadav@gmail.com>
2026-04-30 13:37:34 +0530

    Change to configure Spring security via In Memory Users and User roles . Needs a dependency for spring-security and code config to set up users along with their roles. Added a code review by SonarQube that string literals must not be duplicated

---
+++ 06-spring-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,72 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig  {
+
+    private static final String ROLE_EMPLOYEE = "EMPLOYEE";
+    private static final String ROLE_MANAGER = "MANAGER";
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles(ROLE_EMPLOYEE)
+                .build();
+
+        UserDetails marie = User.builder()
+                .username("marie")
+                .password("{noop}test123")
+                .roles(ROLE_EMPLOYEE , ROLE_MANAGER )
+                .build();
+
+        UserDetails janardan = User.builder()
+                .username("Janardan")
+                .password("{noop}test123")
+                .roles( ROLE_EMPLOYEE , ROLE_MANAGER , "ADMIN")
+                .build();
+
+        return  new InMemoryUserDetailsManager(john, marie, janardan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain( HttpSecurity http) {
+
+
+        try {
+            String employeeIdEndpoint = "/api/employees/**";
+
+            http.authorizeHttpRequests(configurer -> configurer
+                    .requestMatchers(HttpMethod.GET, "/api/employees").hasRole(ROLE_EMPLOYEE)
+                    .requestMatchers(HttpMethod.GET, employeeIdEndpoint).hasRole(ROLE_EMPLOYEE)
+                    .requestMatchers(HttpMethod.POST, "/api/employees").hasRole(ROLE_MANAGER)
+                    .requestMatchers(HttpMethod.PUT, employeeIdEndpoint).hasRole(ROLE_MANAGER)
+                    .requestMatchers(HttpMethod.PATCH, employeeIdEndpoint).hasRole(ROLE_MANAGER)
+                    .requestMatchers(HttpMethod.DELETE, employeeIdEndpoint).hasRole("ADMIN")
+            );
+
+            // use HTTP Basic authentication
+            http.httpBasic(Customizer.withDefaults());
+
+            // disable CSRF (cross site request forgery)
+            // in general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+            http.csrf(csrf -> csrf.disable());
+
+            return http.build();
+        } catch (Exception e) {
+            throw new IllegalStateException("Failed to build Spring Security Filter chain" , e);
+        }
+
+    }
+}
